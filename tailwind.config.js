03ac9dfc631cd61ecb567f0642e53198034a2ad7
Ayman Sedqi <sedqiayman02@gmail.com>
2024-05-26 12:12:56 +0100

    Set up project

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors : {
+          veryDarkBlue : "hsl(233, 47%, 7%)",
+          DarkDesBlue : "hsl(244, 38%, 16%)",
+          SoftViolet : "hsl(277, 64%, 61%)",
+          SlightTranspWhite : "hsla(0, 0%, 100%, 0.75)",
+          SlightTranspWhiteTitle : "hsla(0, 0%, 100%, 0.6)"
+      }
+    },
   },
   plugins: [],
 }
